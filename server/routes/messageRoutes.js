f4efe61d4afb097aa33a1d80289a3e84ce5387c7
oroyinloye <richard.oyinloye@gmail.com>
2025-10-30 16:10:10 +0100

    Add message routes for posting messages

---
+++ server/routes/messageRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const Message = require('../models/Message');
+
+router.post('/messages', async (req, res) => {
+  try {
+    const { sender, content } = req.body;
+    const newMessage = new Message({ sender, content });
+    await newMessage.save();
+    res.status(201).json(newMessage);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to save message' });
+  }
+});
+
+module.exports = router;
